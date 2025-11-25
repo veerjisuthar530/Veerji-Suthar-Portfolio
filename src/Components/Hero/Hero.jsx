71cfbdce2620a0a72078db17e3f5bc304d18e3b1
veerjisuthar530 <veeramdas530@gmail.com>
2025-11-26 00:18:23 +0500

    cv

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -53,7 +53,7 @@ export default function Hero() {
 
           <div className="mt-6 flex flex-col sm:flex-row sm:items-center sm:gap-4 gap-3 justify-center md:justify-start">
             <a
-              href="public/Veerji Suthar CV.pdf"
+              href="src/assets/Veerji Suthar CV.pdf"
               download
               className="inline-flex items-center justify-center px-5 py-2 border border-transparent text-sm font-medium rounded-md bg-teal-500 text-neutral-900 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-400"
             >
